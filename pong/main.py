f58b6e566af3a7abc3626cfe30f070149226a86b
Mattias Salo <salo.mattias@gmail.com>
2018-07-04 21:24:01 +0200

    Finalized Pong game

--- pong/main.py
+++ pong/main.py
@@ -20,6 +20,8 @@ PADDLE_WIDTH = 10
 PADDLE_SPEED = 5
 LEFT_PADDLE_X = 10
 RIGHT_PADDLE_X = WINDOW_WIDTH - LEFT_PADDLE_X - PADDLE_WIDTH
+WINNING_SCORE = 7
+SPEED_INC_COUNTER = 4
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -35,18 +37,21 @@ def main():
     display_surf = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     pygame.display.set_caption("Pong")
 
+    while True:
+        run_game(display_surf, fps_clock)
+        show_game_over_screen(display_surf, pygame.font.Font("freesansbold.ttf", 18))
+
+
+def run_game(display_surf, fps_clock):
+    """ Runs an entire instance of the game, returns when someone wins. """
     left_score = 0
     right_score = 0
     collision_counter = 0
-    ball_speed = BALL_SPEED
 
-    x_speed, y_speed = random_ball_start()
+    ball = random_ball_start()
 
-    l_paddle_y = WINDOW_HEIGHT / 2 - PADDLE_LENGTH / 2
-    r_paddle_y = WINDOW_HEIGHT / 2 - PADDLE_LENGTH / 2
-
-    x = int(WINDOW_WIDTH / 2)
-    y = int(WINDOW_HEIGHT / 2)
+    paddles = {'left': WINDOW_HEIGHT / 2 - PADDLE_LENGTH / 2,
+               'right': WINDOW_HEIGHT / 2 - PADDLE_LENGTH / 2}
 
     while True:  # main game loop
         display_surf.fill(BG_COLOR)
@@ -56,36 +61,26 @@ def main():
                 pygame.quit()
                 sys.exit()
 
-        l_paddle_y, r_paddle_y = move_paddles(l_paddle_y, r_paddle_y)
+        paddles = move_paddles(paddles)
+        ball = move_ball(ball)
 
-        x = x + x_speed
+        # create rectangles for easier collision detection
+        ball_rect = pygame.Rect(ball['x'], ball['y'], BALL_SIDE, BALL_SIDE)
+        l_paddle_rect = pygame.Rect(LEFT_PADDLE_X, paddles[LEFT], PADDLE_WIDTH, PADDLE_LENGTH)
+        r_paddle_rect = pygame.Rect(RIGHT_PADDLE_X, paddles[RIGHT], PADDLE_WIDTH, PADDLE_LENGTH)
 
-        new_y = y + y_speed
-        if new_y > WINDOW_HEIGHT - BALL_SIDE or new_y < 0:
-            y_speed *= -1
-        y = y + y_speed
+        collision = collision_detect(ball_rect, l_paddle_rect, r_paddle_rect)
 
-        ball_rect = pygame.Rect(x, y, BALL_SIDE, BALL_SIDE)
-        l_paddle_rect = pygame.Rect(LEFT_PADDLE_X, l_paddle_y, PADDLE_WIDTH, PADDLE_LENGTH)
-        r_paddle_rect = pygame.Rect(RIGHT_PADDLE_X, r_paddle_y, PADDLE_WIDTH, PADDLE_LENGTH)
-
-        collision = collision_detect(ball_rect, l_paddle_rect, r_paddle_rect, x_speed, y_speed, BALL_SPEED)
-
-        if collision == RIGHT:
-            x_speed, y_speed = handle_r_paddle_collision(ball_rect, r_paddle_rect, ball_speed)
-            collision_counter += 1
-        elif collision == LEFT:
-            x_speed, y_speed = handle_l_paddle_collision(ball_rect, l_paddle_rect, ball_speed)
+        if collision:
+            ball['x_speed'], ball['y_speed'] = handle_paddle_collision(ball, paddles, collision)
             collision_counter += 1
 
-        ball_speed = BALL_SPEED + int(collision_counter / 4)
+        ball['speed'] = BALL_SPEED + int(collision_counter / SPEED_INC_COUNTER)
 
-        if check_score(ball_rect):
-            x_speed, y_speed = random_ball_start()
-            x, y = WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2
-            ball_speed = BALL_SPEED
+        if is_point(ball_rect):  # update score if needed
+            ball = random_ball_start()
             collision_counter = 0
-            if check_score(ball_rect) == LEFT:
+            if is_point(ball_rect) == LEFT:
                 left_score += 1
             else:
                 right_score += 1
@@ -93,18 +88,15 @@ def main():
 
         draw_game(display_surf, left_score, right_score, ball_rect, l_paddle_rect, r_paddle_rect)
 
-        if left_score == 7 or right_score == 7:
-            show_game_over_screen(display_surf, pygame.font.Font("freesansbold.ttf", 18))
-            left_score = 0
-            right_score = 0
-            l_paddle_y = WINDOW_HEIGHT / 2 - PADDLE_LENGTH / 2
-            r_paddle_y = WINDOW_HEIGHT / 2 - PADDLE_LENGTH / 2
+        if left_score == WINNING_SCORE or right_score == WINNING_SCORE:  # game over
+            return
 
         fps_clock.tick(FPS)
 
 
 def draw_game(display_surf, left_score, right_score, ball_rect, l_paddle_rect, r_paddle_rect):
-    font = pygame.font.Font("freesansbold.ttf", 18)
+    """ Draws the entirety of the game. """
+    font = pygame.font.Font("freesansbold.ttf", 25)
     left_score_surf = font.render("%s" % left_score, True, WHITE)
     left_score_rect = left_score_surf.get_rect()
     left_score_rect.topleft = (0, 0)
@@ -121,6 +113,16 @@ def draw_game(display_surf, left_score, right_score, ball_rect, l_paddle_rect, r
     pygame.display.update()
 
 
+def move_ball(ball):
+    """ Moves the ball one step. """
+    new_y = ball['y'] + ball['y_speed']
+    if new_y > WINDOW_HEIGHT - BALL_SIDE or new_y < 0:
+        ball['y_speed'] *= -1
+    ball['y'] += ball['y_speed']
+    ball['x'] += ball['x_speed']
+    return ball
+
+
 def draw_ball(display_surf, ball_rect):
     pygame.draw.rect(display_surf, WHITE, ball_rect)
 
@@ -129,33 +131,52 @@ def draw_paddle(display_surf, paddle_rect):
     pygame.draw.rect(display_surf, WHITE, paddle_rect)
 
 
-def move_paddles(l_paddle_y, r_paddle_y):
+def move_paddles(paddles):
+    """
+    Moves the paddles according to the buttons pressed.
+    A and D moves the left paddle, and UP and DOWN moves
+    the right.
+    """
     keys = pygame.key.get_pressed()
     if keys[K_w]:
-        new_paddle_y = l_paddle_y - PADDLE_SPEED
-        l_paddle_y = new_paddle_y if new_paddle_y > -PADDLE_LENGTH else -PADDLE_LENGTH
+        new_paddle_y = paddles[LEFT] - PADDLE_SPEED
+        paddles[LEFT] = new_paddle_y if new_paddle_y > -PADDLE_LENGTH else -PADDLE_LENGTH
     elif keys[K_s]:
-        new_paddle_y = l_paddle_y + PADDLE_SPEED
-        l_paddle_y = new_paddle_y if new_paddle_y < WINDOW_HEIGHT else WINDOW_HEIGHT
+        new_paddle_y = paddles[LEFT] + PADDLE_SPEED
+        paddles[LEFT] = new_paddle_y if new_paddle_y < WINDOW_HEIGHT else WINDOW_HEIGHT
     if keys[K_UP]:
-        new_paddle_y = r_paddle_y - PADDLE_SPEED
-        r_paddle_y = new_paddle_y if new_paddle_y > -PADDLE_LENGTH else -PADDLE_LENGTH
+        new_paddle_y = paddles[RIGHT] - PADDLE_SPEED
+        paddles[RIGHT] = new_paddle_y if new_paddle_y > -PADDLE_LENGTH else -PADDLE_LENGTH
     elif keys[K_DOWN]:
-        new_paddle_y = r_paddle_y + PADDLE_SPEED
-        r_paddle_y = new_paddle_y if new_paddle_y < WINDOW_HEIGHT else WINDOW_HEIGHT
-    return l_paddle_y, r_paddle_y
+        new_paddle_y = paddles[RIGHT] + PADDLE_SPEED
+        paddles[RIGHT] = new_paddle_y if new_paddle_y < WINDOW_HEIGHT else WINDOW_HEIGHT
+    return paddles
 
 
 def random_ball_start():
+    """
+    Sets the ball in the middle of the screen and sets a random direction towards either
+    one of the players.
+    """
     angle = random.randint(0, 360)
     while (45 < angle < 135) or (225 < angle < 315):
         angle = random.randint(0, 360)
-    x_speed = int(math.cos(math.radians(angle)) * BALL_SPEED)
-    y_speed = int(math.sin(math.radians(angle)) * BALL_SPEED)
-    return x_speed, y_speed
+    x_speed = math.cos(math.radians(angle)) * BALL_SPEED
+    y_speed = math.sin(math.radians(angle)) * BALL_SPEED
+    return {'x': WINDOW_WIDTH / 2 - BALL_SIDE / 2,
+            'y': WINDOW_HEIGHT / 2 - BALL_SIDE / 2,
+            'x_speed': x_speed,
+            'y_speed': y_speed,
+            'speed': BALL_SPEED}
 
 
-def collision_detect(ball, left_paddle, right_paddle, speed_x, speed_y, speed):
+def collision_detect(ball, left_paddle, right_paddle):
+    """
+    Detects a collision between the paddles and the ball and returns a value corresponding to the collision
+    or lack of collision.
+
+    :return: RIGHT, LEFT or False
+    """
     if ball.colliderect(right_paddle):
         return RIGHT
     if ball.colliderect(left_paddle):
@@ -163,23 +184,20 @@ def collision_detect(ball, left_paddle, right_paddle, speed_x, speed_y, speed):
     return False
 
 
-def handle_l_paddle_collision(ball, paddle, speed):
-
-    relative_position = (paddle.y + (PADDLE_LENGTH / 2)) - (ball.y + (BALL_SIDE / 2))
+def handle_paddle_collision(ball, paddles, collision):
+    """
+    Changes the direction of the ball depending on which paddle is hit.
+    """
+    relative_position = (paddles[collision] + (PADDLE_LENGTH / 2)) - (ball['y'] + (BALL_SIDE / 2))
     normalised_relative_position = relative_position / (PADDLE_LENGTH / 2)
     bounce = normalised_relative_position * 50
-    return int(math.cos(math.radians(bounce)) * speed), int(-math.sin(math.radians(bounce)) * speed)
+    direction = -1 if collision == RIGHT else 1
+    return (direction * math.cos(math.radians(bounce)) * ball['speed'],
+            -math.sin(math.radians(bounce)) * ball['speed'])
 
 
-def handle_r_paddle_collision(ball, paddle, speed):
-
-    relative_position = (paddle.y + (PADDLE_LENGTH / 2)) - (ball.y + (BALL_SIDE / 2))
-    normalised_relative_position = relative_position / (PADDLE_LENGTH / 2)
-    bounce = normalised_relative_position * 50
-    return int(-math.cos(math.radians(bounce)) * speed), int(-math.sin(math.radians(bounce)) * speed)
-
-
-def check_score(ball):
+def is_point(ball):
+    """ Detects if a point should be rewarded to a player. """
     if ball.x < 0:
         return RIGHT
     elif ball.x + BALL_SIDE > WINDOW_WIDTH:
