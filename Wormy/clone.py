a238cf655fee203e90bfbc426f7d61063bf64908
Mattias Salo <matsa159@student.liu.se>
2018-09-24 10:56:45 +0200

    Updated so that it functions as a game

--- Wormy/clone.py
+++ Wormy/clone.py
@@ -42,6 +42,7 @@ def main():
 
     while True:
         run_game(display_surf, fps_clock)
+        show_game_over(display_surf)
 
 
 def run_game(display_surf, fps_clock):
@@ -51,6 +52,7 @@ def run_game(display_surf, fps_clock):
     snake = [{"x": 2, "y": 0}, {"x": 1, "y": 0}, {"x": 0, "y": 0}]
     apple = random_apple(snake)
     direction = RIGHT
+    new_direction = RIGHT
     wait_frames = START_WAIT_FRAMES
     frame = 0
     score = 0
@@ -58,6 +60,7 @@ def run_game(display_surf, fps_clock):
     # main game loop
     while True:
         display_surf.fill(BLACK)
+        draw_game(display_surf, snake, apple, score)
 
         # Handle exit event, so that game can exit
         for event in pygame.event.get():
@@ -65,14 +68,14 @@ def run_game(display_surf, fps_clock):
                 pygame.quit()
                 sys.exit()
             elif event.type == KEYDOWN:
-                if event.key == K_UP and direction != DOWN:
-                    direction = UP
-                if event.key == K_DOWN and direction != UP:
-                    direction = DOWN
-                if event.key == K_LEFT and direction != RIGHT:
-                    direction = LEFT
-                if event.key == K_RIGHT and direction != LEFT:
-                    direction = RIGHT
+                if event.key == K_UP:
+                    new_direction = UP
+                if event.key == K_DOWN:
+                    new_direction = DOWN
+                if event.key == K_LEFT:
+                    new_direction = LEFT
+                if event.key == K_RIGHT:
+                    new_direction = RIGHT
 
         if frame == wait_frames:
             frame = 0
@@ -81,13 +84,14 @@ def run_game(display_surf, fps_clock):
                 score += 1
                 wait_frames = START_WAIT_FRAMES - score // 2 if START_WAIT_FRAMES - score // 2 > 3 else 3
                 snake.append(snake[-1])
+            direction = next_direction(direction, new_direction)
             move_snake(snake, direction)
             if is_worm_hit(snake):
                 return  # Game over
         else:
             frame += 1
 
-        draw_game(display_surf, snake, apple, score)
+
 
         # This should be on the end of the loop
         pygame.display.update()
@@ -105,6 +109,17 @@ def move_snake(snake, direction):
         snake.insert(0, {"x": snake[0]["x"] + 1, "y": snake[0]["y"]})
     snake.pop()
     return snake
+
+
+def next_direction(direction, new_direction):
+    if direction == RIGHT:
+        return new_direction if new_direction != LEFT else RIGHT
+    if direction == LEFT:
+        return new_direction if new_direction != RIGHT else LEFT
+    if direction == UP:
+        return new_direction if new_direction != DOWN else UP
+    if direction == DOWN:
+        return new_direction if new_direction != UP else DOWN
 
 
 def random_apple(snake):
@@ -147,6 +162,22 @@ def draw_game(display_surf, snake, apple, score):
     score_rect = score_surf.get_rect()
     score_rect.topleft = (0, 0)
     display_surf.blit(score_surf, score_rect)
+
+
+def show_game_over(display_surf):
+    gameover_surf = pygame.font.Font("freesansbold.ttf", 30).render('Game Over', True, WHITE)
+    gameover_rect = gameover_surf.get_rect()
+    gameover_rect.topleft = (WINDOW_WIDTH / 2 - gameover_rect.width / 2, WINDOW_HEIGHT / 2 - gameover_rect.height / 2)
+    display_surf.blit(gameover_surf, gameover_rect)
+    pygame.display.update()
+    pygame.event.get()
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == KEYDOWN:
+                return
 
 
 if __name__ == "__main__":
